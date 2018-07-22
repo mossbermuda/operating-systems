1dd1cd932458ca28253b8bed4745d8f0f5cc5b5f
Peter Sousa <peter.sousa@dal.ca>
2018-07-22 17:45:02 -0300

    Program Takes in arguements
    Sleep function not doing what is needed.
    Pauses whole program instead of individual thread.

--- Assignment 3/assign3.c
+++ Assignment 3/assign3.c
@@ -18,6 +18,80 @@
  #include <pthread.h>
  #include <string.h>
 
+ #define args 3 // Number of arguements should equal 2 with the input file given
+ #define coffeeBreak 10 // Max time for a patient's coffee break
+
+ // Idle function for the doctor thread. This is the doctor's initial function and it called when no patients to help 
+ void *beginNap() {
+ 	printf("Doctor Napping...\n");
+ 	return NULL;
+ }
+
+// Function will occur when a patient obtains a lock for the doctor, making the doctor unable to treat other patients or nap until the treatment is complete
+ void treatPatient() {
+
+ }
+
+// Function initially called by each patient. Waits for a random amount of time then attempts to seek help from the doctor
+ void *getCoffee(void *vargp) {
+ 	int *myid = (int *)vargp;
+ 	int breakLength = rand() % coffeeBreak;
+ 	printf("Patient %d getting coffee for %d seconds\n", *myid, breakLength);
+ 	sleep(breakLength);
+ 	printf("Patient %d break over\n", *myid);
+ 	return NULL;
+ }
+
+// Function to be called after patient is finished having coffee. 
+// The patient will enter the queue of waiting chairs, if they are first in the queue they will seek treatment when the doctor is available
+ void enterWaitingRoom() {
+ 	
+ }
+
+// Patient will obtain a lock for the doctor and will get treatment for some random period of time. Once complete they will return to having coffee.
+ void getTreatment() {
+ 	
+ }
+
  int main(int argc, char **argv) {
+ 	pthread_t thread;
+ 	int patientCount;
+ 	int seatCount;
+ 	int threadID;
+ 	char buffer[16];
+ 	//Check if 2 arguements have been added to the program before continuing
+ 	if (argc == args) {
+ 		// Collect and store the inputted argument values
+ 		strcpy(buffer, argv[1]);
+ 		patientCount = atoi(buffer);
+ 		bzero(buffer, 16);
+ 		strcpy(buffer, argv[2]);
+ 		seatCount = atoi(buffer);
+ 		// Continue if values for patient and seat count are valid
+ 		if(patientCount > 0 && seatCount > 0) {
+ 			// Print argument values
+ 			printf("Patient Count: %d\nSeat Count: %d\n", patientCount, seatCount);
+
+ 			//Initalize Doctor pthread
+ 			pthread_create(&thread, NULL, beginNap, (void *)&threadID);
+ 			
+
+ 			//Initalize Patient Threads
+ 			for (threadID = 0; threadID < patientCount; threadID++) {
+ 				pthread_create(&thread, NULL, getCoffee, (void *)&threadID);
+ 				pthread_join(thread, NULL);
+ 			}
+
+ 			pthread_exit(NULL);
+
+ 		}
+ 		else {
+ 			printf("ERROR\nInclude 2 arguements when running program\nArgument 1 - Patient Count (Integer)\nArguement 2 - Seat Count (Integer)\n");
+ 		}
+ 	}
+ 	else {
+ 		printf("ERROR\nInclude 2 arguements when running program\nArgument 1 - Patient Count (Integer)\nArguement 2 - Seat Count (Integer)\n");
+ 	}
+
  	return 0;
  }
